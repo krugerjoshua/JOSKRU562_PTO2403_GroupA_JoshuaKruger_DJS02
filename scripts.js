897db88b3658444b703cb44d10d5416b0dc170fb
Joshua <joshuakruger66@live.com>
2024-10-27 17:16:26 +0200

    Implemented error handlling according to user stories and tested with the following test cases: - Test case 1: Test the error handling when the user enters a non-numeric value. - Test case 2: Test the error handling when when values are missing. - Test case 3: Test the error handling when output is a decimal number. - Test case 4: Test the error handling when user inputs an invalid division.

--- scripts.js
+++ scripts.js
@@ -5,5 +5,25 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  // Dividing numbers when values are missing.
+  if  (dividend === "" || divider === "") {
+    result.innerText = "Please enter both numbers.";
+    return
+  }
+  // An invalid division should log an errer in the console
+  if (divider === "0") {
+    result.innerText = "Division not performed. Invalid number provided. Try again"
+    console.log("Error: Division by zero is not allowed");
+    return
+  }
+
+  // Providing anything that is not a number should crash the program
+  if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerText = "Something critical went wrong. Please reload the page"
+    console.log("Error: Non number value entered")
+  }
+
+  // Dividing numbers result in a decimal number
+  result.innerText = Math.floor(dividend / divider)
 });
